bb6591efae632d989b40eb2dd620a2197f021b07
Adrien Escande <adrien.escande@aist.go.jp>
2022-06-08 13:24:43 +0900

    Fix bug in Levenshtein distance and add doc

--- src/bibmanagement/check/LevenshteinDistance.py
+++ src/bibmanagement/check/LevenshteinDistance.py
@@ -1,4 +1,9 @@
 def distance(word1, word2):
+    '''
+    Compute the Levenshtein distance between 2 words, i.e. the minimum number
+    of letter insertion, deletion or replacement needed to go from one word to
+    the other
+    '''
     n1 = len(word1)
     n2 = len(word2)
     row = [i for i in range(n2 + 1)]
@@ -14,6 +19,6 @@ def distance(word1, word2):
                 l = [e, row[w2], row[w2-1]]
                 row[w2-1] = e
                 e = min(l) + 1
-            row[n2] = e
+        row[n2] = e
             
     return row[n2]
